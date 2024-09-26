6a14137447d3591cf8cca38df422293bbaf48eb2
nikonovone <nikonov.nikita@hotmail.com>
2024-09-26 18:45:31 +0700

    feat: add default augmentation

---
+++ src/data/transform.py
@@ -0,0 +1,22 @@
+import albumentations as albu
+from albumentations.pytorch import ToTensorV2
+
+
+def get_train_transforms(img_width: int, img_height: int) -> albu.Compose:
+    return albu.Compose(
+        [
+            albu.Resize(height=img_height, width=img_width),
+            albu.Normalize(),
+            ToTensorV2(),
+        ],
+    )
+
+
+def get_valid_transforms(img_width: int, img_height: int) -> albu.Compose:
+    return albu.Compose(
+        [
+            albu.Resize(height=img_height, width=img_width),
+            albu.Normalize(),
+            ToTensorV2(),
+        ],
+    )
